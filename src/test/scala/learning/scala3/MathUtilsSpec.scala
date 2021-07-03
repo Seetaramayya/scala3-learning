48cc4039224067c75eb5522acef954086552d465
Seeta Ramayya Vadali <srvadali@gmail.com>
2021-07-03 12:53:35 +0200

    New syntax class without braces with ':' and end

--- src/test/scala/learning/scala3/MathUtilsSpec.scala
+++ src/test/scala/learning/scala3/MathUtilsSpec.scala
@@ -3,10 +3,16 @@ package learning.scala3
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
-class MathUtilsSpec extends AnyWordSpec with Matchers {
+class MathUtilsSpec extends AnyWordSpec with Matchers:
   "Math Utils double" should {
     "return 2 times of given number" in {
       MathUtils.double(2) shouldBe 4
     }
   }
-}
+
+  "Math Utils sum" should {
+    "return added value of the given numbers" in {
+      MathUtils.sum(2, 3) shouldBe 5
+    }
+  }
+end MathUtilsSpec
